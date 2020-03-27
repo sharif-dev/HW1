df9e5e3c4d8226b8fbcbd6ba666e7d8c0b1ec35d
nima <nimxj4141@gmail.com>
2020-03-27 15:30:57 +0430

    WeatherCondition class created

---
+++ app/src/main/java/com/example/weatherforecast/model/WeatherCondition.java
@@ -0,0 +1,52 @@
+package com.example.weatherforecast.model;
+
+public class WeatherCondition {
+    private String summary;
+    private String temperatureHigh;
+    private String temperatureLow;
+    private String pressure;
+    private String humidity;
+
+    public WeatherCondition() {
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getTemperatureHigh() {
+        return temperatureHigh;
+    }
+
+    public void setTemperatureHigh(String temperatureHigh) {
+        this.temperatureHigh = temperatureHigh;
+    }
+
+    public String getTemperatureLow() {
+        return temperatureLow;
+    }
+
+    public void setTemperatureLow(String temperatureLow) {
+        this.temperatureLow = temperatureLow;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+}
