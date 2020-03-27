7d8d14cfe4574dcdc281c0f51672edfa2ac25415
Sepehr Feli <sepehr.feli.1377@gmail.com>
2020-03-27 13:34:27 +0430

    RecyclerItemClickListener class created

--- app/src/main/java/com/example/weatherforecast/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/weatherforecast/adapters/RecyclerViewAdapter.java
@@ -5,13 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.weatherforecast.R;
 import com.example.weatherforecast.model.City;
-
 import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
