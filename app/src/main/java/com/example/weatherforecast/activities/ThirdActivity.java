1e9d89ff40b8d344b09db92fa893241fc00356cc
Sepehr Feli <sepehr.feli.1377@gmail.com>
2020-03-28 02:43:51 +0430

    ThirdActivity just created

---
+++ app/src/main/java/com/example/weatherforecast/activities/ThirdActivity.java
@@ -0,0 +1,16 @@
+package com.example.weatherforecast.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.weatherforecast.R;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
